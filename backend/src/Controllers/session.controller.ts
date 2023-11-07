e2c4095c4ba0aa26d3d253b7f1d04ec7da27d8ad
JokerZ75 <deaconhughes07@gmail.com>
2023-11-07 18:52:08 +0000

    Caching util functions

---
+++ backend/src/Controllers/session.controller.ts
@@ -0,0 +1,3 @@
+import { redisClient } from "..";
+import jwt from "jsonwebtoken";
+
---
+++ backend/src/utils/cache.ts
@@ -0,0 +1,35 @@
+import { redisClient } from "..";
+import { Request, Response, NextFunction } from "express";
+
+const cacheGetRequest = (key: string, cb: Function) => {
+  return new Promise((resolve, reject) => {
+    redisClient.get(key).then(async (data: any) => {
+      if (data != null) {
+        return resolve(JSON.parse(data));
+      } else {
+        const freshData = await cb();
+        redisClient.setEx(key, 3600, JSON.stringify(freshData));
+        resolve(freshData);
+      }
+    });
+  });
+};
+
+const setCacheWithExpire = (key: string, data: any, expire: number) => {
+  redisClient.setEx(key, expire, JSON.stringify(data));
+};
+
+const clearCache = (key: string) => {
+  redisClient.del(key);
+};
+
+const clearAllCache = () => {
+  redisClient.FLUSHALL();
+};
+
+const setCachePermanent = (key: string, data: any) => {
+  redisClient.set(key, JSON.stringify(data));
+};
+
+
+export { cacheGetRequest, setCacheWithExpire, clearCache, clearAllCache, setCachePermanent };
