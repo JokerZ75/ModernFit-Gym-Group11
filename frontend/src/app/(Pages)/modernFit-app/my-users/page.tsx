f822a28e6602fded5f5e7a8ee8b6f17902069268
AlexDobson164 <alexdobson04@gmail.com>
2023-11-25 21:43:07 +0000

    the design for "my-user" page, didnt make desktop

---
+++ frontend/src/app/(Pages)/modernFit-app/my-users/page.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Button } from "@/app/components/UI/Button";
+import AutoComplete from "@/app/components/UI/AutoComplete";
+import SearchBar from "./components/searchBar";
+import AssignedUser from "./components/assignedUser";
+import UnassignedUser from "./components/unassignedUser";
+
+const myUsers: React.FC = () => {
+    let profileImage = "https://placehold.co/300x300"
+    let firstName = "John"
+    let lastName = "Smith"
+    let height = "5'8"
+    let weight = 165
+    let goals = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean massa magna, viverra sed venenatis vitae, blandit vel mi. Donec non."
+    if (goals.length > 30)
+    {
+        goals = goals.slice(0, 30);
+        goals += "...";
+    }
+
+
+    return (
+        <>
+            <div className="text-blue-200 w-3/4 mx-auto mt-4 mb-4 ">
+                <div className="font-bold text-2xl">
+                    my users
+                </div>
+                <div className="">
+                    <SearchBar />
+                </div>
+                <AssignedUser
+                firstName={firstName}
+                lastName={lastName}
+                profileImage={profileImage}
+                goals={goals}
+                height={height}
+                weight={weight}
+                />
+
+                <div className="font-bold text-2xl">
+                    waiting users
+                </div>
+                <UnassignedUser
+                firstName={firstName}
+                lastName={lastName}
+                profileImage={profileImage}
+                goals={goals}
+                height={height}
+                weight={weight}
+                />
+            </div>
+        </>
+    )
+
+};
+
+export default myUsers;
